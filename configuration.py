886fa7b81603b5073a121a19de3ad27f8f10836c
Natasha Dorogova <natashadorogova@gmail.com>
2024-07-12 15:38:30 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://a53185d2-c73d-4027-9d17-a4319432a2d2.serverhub.praktikum-services.ru"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Natalia",
+    "lastName": "Dorogova",
+    "address": "Moscow, Lenin str, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 800 555 55 55",
+    "rentTime": 5,
+    "deliveryDate": "2024-07-06",
+    "comment": "hot pizzzza",
+    "color": [
+        "BLACK"
+    ]
+}
